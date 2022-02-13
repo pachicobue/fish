40247c8d44e8bf8905f167493b7ee73096e83969
Pachicobue <tigerssho@gmail.com>
2022-02-13 22:59:57 +0900

    PATH設定追加＆コンパイルコマンド修正

--- conf.d/compilation.fish
+++ conf.d/compilation.fish
@@ -1,17 +1,16 @@
-set ALGOLIB_PATH '/home/sho/programming/algolib'
+set ALGOLIB_PATH $HOME/programming/contest/algolib
+set ACL_PATH     $HOME/programming/contest/ac-library
 
 function dg++
-    set library_path $ALGOLIB_PATH
-    set source_path '${library_path}/src'
+    set library_paths $ALGOLIB_PATH/src $ACL_PATH
     set source_name $argv[1]
     set binary_name (string replace -r '(.*)\.cpp' '$1.exe' $source_name)
-    command g++ -O0 -g3 -std=c++17 -fsanitize=undefined -D_GLIBCXX_DEBUG -DHOGEPACHI -lpthread -I$source_path $source_name -o $binary_name
+    command g++ -O0 -g3 -std=c++17 -fsanitize=undefined -D_GLIBCXX_DEBUG -DHOGEPACHI -lpthread -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
 end
 
 function fg++
-    set library_path $ALGOLIB_PATH
-    set source_path '${library_path}/src'
+    set library_paths $ALGOLIB_PATH/src $ACL_PATH
     set source_name $argv[1]
     set binary_name (string replace -r '(.*)\.cpp' '$1.exe' $source_name)
-    command g++ -Ofast -std=c++17 -DHOGEPACHI -lpthread -I$source_path $source_name -o $binary_name
+    command g++ -Ofast -std=c++17 -DHOGEPACHI -lpthread -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
 end
--- conf.d/env.fish
+++ conf.d/env.fish
@@ -1,2 +1,3 @@
-set -U fish_user_paths ~/.local/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 set EDITOR 'emacs -nw'
