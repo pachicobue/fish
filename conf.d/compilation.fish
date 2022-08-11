f1639bd9b7acb31c4037664d13a5f54bb9c9db49
Pachicobue <tigerssho@gmail.com>
2022-08-12 02:49:03 +0900

    コンパイルオプション変更

--- conf.d/compilation.fish
+++ conf.d/compilation.fish
@@ -5,12 +5,26 @@ function dg++
     set library_paths $ALGOLIB_PATH/src $ACL_PATH
     set source_name $argv[1]
     set binary_name (string replace -r '(.*)\.cpp' '$1.exe' $source_name)
-    command g++ -O0 -g3 -std=c++17 -fsanitize=undefined -D_GLIBCXX_DEBUG -DHOGEPACHI -lpthread -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
+    command g++ -Wall -Wextra -Wno-stringop-overflow -O0 -g3 -std=c++17 -fsanitize=undefined -D_GLIBCXX_DEBUG -DHOGEPACHI -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
 end
 
 function fg++
     set library_paths $ALGOLIB_PATH/src $ACL_PATH
     set source_name $argv[1]
     set binary_name (string replace -r '(.*)\.cpp' '$1.exe' $source_name)
-    command g++ -Ofast -std=c++17 -DHOGEPACHI -lpthread -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
+    command g++ -Wall -Wextra -Wno-stringop-overflow -Ofast -std=c++17 -DHOGEPACHI -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
+end
+
+function dgch
+    set library_paths $ALGOLIB_PATH/src $ACL_PATH
+    set source_name $argv[1]
+    set binary_name $source_name.dg++.gch
+    command g++ -Wall -Wextra -Wno-stringop-overflow -O0 -g3 -std=c++17 -fsanitize=undefined -D_GLIBCXX_DEBUG -DHOGEPACHI -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
+end
+
+function fgch
+    set library_paths $ALGOLIB_PATH/src $ACL_PATH
+    set source_name $argv[1]
+    set binary_name $source_name.fg++.gch
+    command g++ -Wall -Wextra -Wno-stringop-overflow -Ofast -std=c++17 -DHOGEPACHI -I$library_paths[1] -I$library_paths[2] $source_name -o $binary_name
 end
