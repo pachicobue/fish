6225290dcc406f12c7670514848401017220085e
Pachicobue <tigerssho@gmail.com>
2023-03-01 03:01:50 +0900

    ちょいと変更

--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -1,7 +1,2 @@
-alias e='emacs -nw'
 alias open='explorer.exe'
-
-alias g++='g++ -std=c++17 -Wall -Wextra -Wshadow'
-alias clang++='clang++ -std=c++1z -Wall -Wextra -Wshadow'
-
 alias reload='exec fish'
--- conf.d/env.fish
+++
@@ -1,3 +0,0 @@
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-set EDITOR 'emacs -nw'
