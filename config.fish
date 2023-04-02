8757aa675747920513612ba407b1f56d923e304b
Pachicobue <tigerssho@gmail.com>
2023-04-02 14:28:16 +0900

    ssh-agent設定を一旦削除

--- config.fish
+++ config.fish
@@ -1,13 +1,3 @@
-set SSH_AGENT_FILE "$HOME/.ssh/ssh_agent"
-test -f $SSH_AGENT_FILE; and source $SSH_AGENT_FILE > /dev/null 2>&1
-if not ssh-add -l > /dev/null 2>&1
-  if not ps -ef | grep -v grep | grep ssh-agent
-    eval (ssh-agent -c > $SSH_AGENT_FILE 2>&1)
-  end
-  source $SSH_AGENT_FILE > /dev/null 2>&1
-  find $HOME/.ssh -name id_ed25519 | xargs ssh-add
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
