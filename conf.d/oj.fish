831538cdb82bc51cfa5872694c008e8d81859025
Pachicobue <tigerssho@gmail.com>
2023-11-18 15:40:55 +0900

    oj更新

--- conf.d/oj.fish
+++ conf.d/oj.fish
@@ -2,9 +2,9 @@ set ALGOLIB_PATH /home/sho/programming/oj-environment/algolib
 set ACL_PATH /home/sho/programming/oj-environment/ac-library
 
 set CXX_COMPILER g++
-set CXX_COMMON_OPTIONS -std=c++17 -Wall -Wextra -Wno-stringop-overflow -I$ALGOLIB_PATH/src -I$ACL_PATH
-set CXX_DEBUG_OPTIONS -O0 -g3 -fsanitize=undefined -D_GLIBCXX_DEBUG -DHOGEPACHI
-set CXX_RELEASE_OPTIONS -Ofast -DHOGEPACHI
+set CXX_COMMON_OPTIONS -std=gnu++20 -Wall -Wextra -I$ALGOLIB_PATH/src -I$ACL_PATH
+set CXX_DEBUG_OPTIONS -O0 -g3 -fsanitize=undefined,address -DHOGEPACHI
+set CXX_RELEASE_OPTIONS -O2 -DHOGEPACHI
 
 function dg++
     set source_name $argv[1]
